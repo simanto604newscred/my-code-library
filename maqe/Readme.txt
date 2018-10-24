6993add00fe29ff15d728f427025e839e9558bb3
simanto <simanto605@gmail.com>
2018-10-24 18:15:04 +0600

    finalize make to send

---
+++ maqe/Readme.txt
@@ -0,0 +1,16 @@
+To run the section2 http://maqe.github.io/maqe-bot.html solution:
+
+    *   From terminal "$ python section2.py RW15RW1"
+
+
+
+ To run the Template challenge http://maqe.github.io/template.html solution:
+
+    *   Install dependencies from terminal "$ pip install -r requirements.txt"
+    *   Run the server  "$ python run.py"
+    *   Goto browser and hit the url mentioned on the terminal
+    *   Default is http://127.0.0.1:5000/
+
+
+Thanks -
+MD ABUL KASEM KHAN SIMANTO
--- maqe/run.py
+++ maqe/run.py
@@ -1,10 +1,13 @@
 __author__ = 'MAK'
-from flask import Flask, render_template
+
 import requests
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 AUTHOR_URL = 'http://maqe.github.io/json/authors.json'
 POSTS_URL = 'http://maqe.github.io/json/posts.json'
+
 
 @app.route("/")
 def template_test():
@@ -14,16 +17,16 @@ def template_test():
         authors = requests.get(AUTHOR_URL)
         posts = requests.get(POSTS_URL)
 
-    except:
+    except requests.exceptions.RequestException as e:
         errors.append(
-            "Unable to get URL. Please make sure it's valid and try again."
+            "Unable to get URL. Please make sure it's valid and try again.\n{}".format(e)
         )
-
 
     authors_dicts = authors.json()
 
     authors_map = {author_info["id"]: author_info for author_info in authors_dicts}
 
+    # Injecting authors in posts , keeping it here instead of template to reduce maintenance cost
     posts_map = [dict(author_details=authors_map.get(post.get('author_id')),
                       **post) for post in posts.json()]
 
